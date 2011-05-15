3694cba3ee8d1c8d8ed5a6b143a4b186180d67f0
Daniel C. Sobral <dcsobral@gmail.com>
2011-05-14 23:23:12 -0300

    Replace all occurences of n with k

--- src/main/scala/KMin.scala
+++ src/main/scala/KMin.scala
@@ -1,20 +1,20 @@
 object KMin { 
-    def kMin(a1: Array[Int], a2: Array[Int], n: Int): Int = {
-      def select(n2: Int) = n2 match {
-        case `n` => a2(n - 1)
-        case 0   => a1(n - 1)
-        case _   => a1(n - n2 - 1) max a2(n2 - 1)
+    def kMin(a1: Array[Int], a2: Array[Int], k: Int): Int = {
+      def select(k2: Int) = k2 match {
+        case `k` => a2(k - 1)
+        case 0   => a1(k - 1)
+        case _   => a1(k - k2 - 1) max a2(k2 - 1)
       }
       
       def recurse(top: Int, bottom: Int): Int = {
         if (top == bottom) select(top)
         else {
           val x = (bottom + top) / 2 max bottom + 1
-          if (a1(n - x) <= a2(x - 1)) recurse (x - 1, bottom)
+          if (a1(k - x) <= a2(x - 1)) recurse (x - 1, bottom)
           else recurse(top, x)
         }
       }
       
-      recurse(n min a2.size, 0 max n - a1.size)
+      recurse(k min a2.size, 0 max k - a1.size)
     }
 }
