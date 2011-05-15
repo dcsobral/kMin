1e760387d0a9864f52564465b13565135f851651
Daniel C. Sobral <dcsobral@gmail.com>
2011-05-14 22:10:53 -0300

    The algorithm can handle empty arrays. Just guarantee that at least one array is non-empty, because, otherwise, kMin would need to be changed to return Option[Int].

--- src/test/scala/KMinTest.scala
+++ src/test/scala/KMinTest.scala
@@ -4,7 +4,7 @@ import KMin._
 
 object KMinTest extends Properties("logN KMin") {
     property("kMin finds the kth element of two sorted arrays") = forAll { (a1: Array[Int], a2: Array[Int], n: Int) => 
-        (a1.size > 0 && a2.size > 0 && n >= 0) ==> { 
+        (a1.size + a2.size > 0 && n >= 0) ==> { 
             val k = n % (a1.size + a2.size) + 1
             kMin(a1.sorted, a2.sorted, k) == (a1 ++ a2).sorted.apply(k - 1) 
         } :| "val a1 = Array(%s)\nval a2 = Array(%s)\nval n = %d\nval kMin = %d\n".format(
