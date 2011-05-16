a9c8c3efb4a511ce34bbd40abc92082c9bc6da9e
Daniel C. Sobral <dcsobral@gmail.com>
2011-05-15 22:51:10 -0300

    Use %% notation to automatically append Scala version.

--- project/build/KMinProject.scala
+++ project/build/KMinProject.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class KMinProject(info: ProjectInfo) extends DefaultProject(info)
 {
-    val scalacheck = "org.scala-tools.testing" % "scalacheck_2.9.0" % "1.9" % "test" 
+    val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test" 
 }
